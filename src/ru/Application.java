23803350fdad9c99cb5797a545bfc12562d3baa1
Ilya Azanov <ilya.azanov@mail.ru>
2019-11-23 17:07:02 +0300

    word connection

--- src/ru/Application.java
+++ src/ru/Application.java
@@ -3,7 +3,7 @@ package ru;
 public class Application {
 
     public  static  void  main (String[] args){
-        System.out.println("Hello World!");
+        System.out.println("Hello hell!");
     }
 
 }
